f6c6306266ba8b202406583d336c283dde253fbb
Michael Asnes <mike.asnes@gmail.com>
2015-02-26 23:44:23 -0700

    added simple net_increase calculator for compounding over time

---
+++ net_increase.py
@@ -0,0 +1,23 @@
+'''
+Calcate net increase
+By: Michael Asnes
+'''
+import sys
+
+def usage():
+    sys.exit("""Usage:
+\tpython net_increase.py <rate of increase> <periods>""")
+
+def main():
+    try:
+        rate = float(sys.argv[1])
+        periods = int(sys.argv[2])
+    except IndexError:
+        usage()
+    acc = 1
+    for _ in range(periods):
+        acc *= rate
+    print("Increased {:.3f} times".format(acc))
+
+if __name__ == '__main__':
+    main()
